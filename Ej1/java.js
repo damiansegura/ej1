06a7c1e458a564c9020a13c0cde28e75a9a0584f
Juan Manuel Lopez <tandiljuan@gmail.com>
2014-08-12 09:28:33 -0300

    Format, indent and add missing colons.

--- Ej1/java.js
+++ Ej1/java.js
@@ -1,31 +1,27 @@
-
-
-
 function footbar(id) {
-var fot = document.getElementById('footer').getElementsByTagName('li')
+    var fot = document.getElementById('footer').getElementsByTagName('li');
 
-	for (var i = 0; i < fot.length; i++) {
+    for (var i = 0; i < fot.length; i++) {
 
-	fot[i].className= "lista2";
+        fot[i].className= "lista2";
 
+    }
+
+    document.getElementById(id).className= "lista2_1";
 }
-document.getElementById(id).className= "lista2_1";
-
-}
-
-
 
 function myFunction(id) {
-var fot = document.getElementById('menu').getElementsByTagName('li')
+    var fot = document.getElementById('menu').getElementsByTagName('li');
 
-	for (var i = 0; i < fot.length; i++) {
+    for (var i = 0; i < fot.length; i++) {
 
-if	(fot[i].className ="selected_1") {
-fot[i].className= "selected";
- 
-//fot[i].className.substring= (0,-2);
-} 
-}
-document.getElementById(id).className += "_1";
+        if (fot[i].className == "selected_1") {
 
+            fot[i].className= "selected";
+
+            //fot[i].className.substring= (0,-2);
+        }
+    }
+
+    document.getElementById(id).className += "_1";
 }
